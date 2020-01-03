e7f5959965d18e9a4a42586a7341d0f1a60afa6c
jsorigin <origin@win.com>
2020-01-03 11:57:28 +0800

    feat: 不规则的底部导航

--- lib/BottomNav32.dart
+++ lib/BottomNav32.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import './views/each_view.dart';
 
 class BottomAppBarDemo extends StatefulWidget {
   _BottomAppBarDemoState createState() => _BottomAppBarDemoState();
 }
 
 class _BottomAppBarDemoState extends State<BottomAppBarDemo> {
+  List<Widget> _eachView;  //创建视图数组
+  int _index = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _eachView = List();
+    _eachView..add(EachView('Home'))..add(EachView('Me'));
+  }
 
   @override
   Widget build(BuildContext context) {
      return Scaffold(
         floatingActionButton: FloatingActionButton(
           onPressed: (){
-
+            Navigator.of(context).push(MaterialPageRoute(builder:(BuildContext context){
+              return EachView('New Page');
+            }));
           },
           tooltip: 'Increment',
           child: Icon(
@@ -31,20 +43,23 @@ class _BottomAppBarDemoState extends State<BottomAppBarDemo> {
                icon:Icon(Icons.home),
                color:Colors.white,
                onPressed:(){
-
+                setState(() {
+                _index=0;             
+                });
                }
              ),
              IconButton(
                icon:Icon(Icons.airport_shuttle),
                color:Colors.white,
                onPressed:(){
-
+                setState(() {
+                _index=1;             
+                });
                }
              ),
            ],
          ),
-       )
-        ,
+       ),
      );
   }
 }
